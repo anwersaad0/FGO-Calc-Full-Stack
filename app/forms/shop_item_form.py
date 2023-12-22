fac3d31ebe69757c51b9519fe6ae2bb1d0599d4d
anwersaad0 <anwer.saad0@gmail.com>
2023-12-21 23:14:52 -0800

    restarted local db, began work on forms

---
+++ app/forms/shop_item_form.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField
+from wtforms.validators import DataRequired, ValidationError
+
+from app.models import ShopItem
+
+def item_exists(form, field):
+    new_item_name = field.data
+    item = ShopItem.query.filter(ShopItem.name == new_item_name).first()
+    if item:
+        raise ValidationError("This item already exists!")
+
+class NewItem(FlaskForm):
+    name = StringField("Item Name", validators=[DataRequired()])
+    price = FloatField("Item Price", validators=[DataRequired()])
