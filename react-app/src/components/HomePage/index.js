8677144682a103c7944dc7f815624159db68133c
anwersaad0 <anwer.saad0@gmail.com>
2024-03-20 19:20:59 -0700

    added homepage file

---
+++ react-app/src/components/HomePage/index.js
@@ -0,0 +1,10 @@
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useHistory } from "react-router-dom";
+import { useEffect } from "react";
+
+function HomePage() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+}
+
+export default HomePage;
