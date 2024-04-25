4686dff5ac091679cd30613daaa75ed2e53e8082
anwersaad0 <anwer.saad0@gmail.com>
2024-04-25 10:01:21 -0700

    starting changes to playmediaa

--- react-app/src/components/PlayMedia/index.js
+++ react-app/src/components/PlayMedia/index.js
@@ -1,3 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom";
 import { useEffect } from "react";
+
+function PlayMediaPage() {
+    const dispatch = useDispatch();
+
+    return (
+        <main>
+
+            <div>
+
+                Media Here
+
+            </div>
+
+        </main>
+    )
+}
