7549069bb7486089e59ee4fffc5dd669528e2fcb
anwersaad0 <anwer.saad0@gmail.com>
2024-04-29 18:37:19 -0700

    adding useEffect for media page

--- react-app/src/components/PlayMedia/index.js
+++ react-app/src/components/PlayMedia/index.js
@@ -1,16 +1,32 @@
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useHistory } from "react-router-dom";
+import { NavLink, useHistory, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { getOneMediaThunk } from "../../store/media";
 
 function PlayMediaPage() {
     const dispatch = useDispatch();
+    const {mediaId} = useParams();
+
+    const history = useHistory();
+
+    useEffect(() => {
+        dispatch(getOneMediaThunk(mediaId));
+    }, [mediaId]);
+
+    // if media doesn't exist return null
 
     return (
         <main>
 
             <div>
 
-                Media Here
+                <div>
+                    Media Title Here
+                </div>
+
+                <div>
+                    Media contents here
+                </div>
 
             </div>
 
