9a7984b29f7f53b62765f934f84e65591871baf2
anwersaad0 <anwer.saad0@gmail.com>
2024-04-30 15:05:22 -0700

    added file to web bucket to test through seeder

--- app/seeds/media.py
+++ app/seeds/media.py
@@ -3,7 +3,12 @@ from sqlalchemy.sql import text
 
 def seed_media():
     example = Media(
-        name="example media", type="comic", ip="example ip", desc="example desciption", url="", creator_id=1
+        name="example media", 
+        type="video", 
+        ip="example ip", 
+        desc="example desciption", 
+        url="https://brainfreezerrr-media.s3.amazonaws.com/VTOL_VR_has_a_beautiful_and_complex.mp4", 
+        creator_id=1
     )
 
     db.session.add(example)
