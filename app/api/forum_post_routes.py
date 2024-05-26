2986c028f61469b947cbc65d38f6c72e07be3ca2
anwersaad0 <anwer.saad0@gmail.com>
2024-05-25 20:43:08 -0700

    starting route methods for forum posts

--- app/api/forum_post_routes.py
+++ app/api/forum_post_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
-from app.models import ForumPost
+from flask_login import current_user, login_required
+from app.models import User, ForumPost
 
 forum_post_routes = Blueprint('posts', __name__)
 
@@ -12,3 +13,13 @@ def all_posts():
 def get_post(id):
     post = ForumPost.query.get(id)
     return post.to_dict()
+
+@forum_post_routes('/new', methods=["POST"])
+@login_required
+def create_post():
+
+    #form
+
+
+
+    return {'error': form.errors}
