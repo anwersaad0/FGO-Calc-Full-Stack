971f414c2ac0f4602ddbd928c607c3e2d25ef679
anwersaad0 <anwer.saad0@gmail.com>
2024-07-01 11:51:31 -0700

    added forumReducer to store index

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import mediaReducer from './media';
+import forumPostReducer from './forum';
 
 const rootReducer = combineReducers({
   session,
   media: mediaReducer,
+  forum: forumPostReducer,
 });
 
 
