98e71961c8e9ca5edc53662b55b3661dc6e70fd5
anwersaad0 <anwer.saad0@gmail.com>
2024-07-13 19:27:37 -0700

    set up base for main forum page

--- react-app/src/components/ForumPage/index.js
+++ react-app/src/components/ForumPage/index.js
@@ -1,2 +1,50 @@
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { allPostsThunk } from "../../store/forum";
+
+function ForumPage() {
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => Object.values(state.posts));
+
+    const [query, setQuery] = useState("");
+
+    useEffect(() => {
+        dispatch(allPostsThunk());
+    }, [dispatch]);
+
+    if (!posts) return null;
+
+    return (
+        <main className="forum-page-root">
+
+            <div>
+
+                <div className="forum-posts-container">
+
+                    {posts?.toReversed().filter(post => {
+                        if (query === '') {
+                            return post;
+                        } else if (post.title.toLowerCase().includes(query.toLocaleLowerCase())) {
+                            return post;
+                        }
+                    }).map(({title}) => (
+                        <NavLink className='post-mini-details' exact to={`/forum/posts/${id}`}>
+
+                            <div>
+
+                                {title}
+
+                            </div>
+
+                        </NavLink>
+                    ))}
+
+                </div>
+
+            </div>
+
+        </main>
+    )
+}
