e78a5295281cbac95cdccaf9c8bcb0d24ee818dc
anwersaad0 <anwer.saad0@gmail.com>
2024-08-26 10:44:58 -0700

    started base for the forum post upload function

--- react-app/src/components/UploadForumPost/index.js
+++ react-app/src/components/UploadForumPost/index.js
@@ -1,1 +1,42 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+
+function UploadForumPost() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    //
+
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+
+    const [valErrs, setValErrs] = useState([]);
+    const [hasSubbed, setHasSubbed] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setHasSubbed(true);
+    }
+
+    return (
+        <main>
+
+            <div>
+
+                <h1> Upload Forum Post </h1>
+
+            </div>
+
+            <form onSubmit={"n"}>
+
+
+
+            </form>
+
+        </main>
+    )
+}
