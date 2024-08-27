f8daa2495c5378b3c29734c774e11a15d5c24ffb
anwersaad0 <anwer.saad0@gmail.com>
2024-08-26 17:34:55 -0700

    added handlesubmit to create post

--- react-app/src/components/UploadForumPost/index.js
+++ react-app/src/components/UploadForumPost/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { createPostThunk } from "../../store/forum";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
@@ -8,7 +8,7 @@ function UploadForumPost() {
     const dispatch = useDispatch();
     const history = useHistory();
 
-    //
+    const sessionUser = useSelector(state => state.session.user);
 
     const [title, setTitle] = useState("");
     const [text, setText] = useState("");
@@ -20,6 +20,20 @@ function UploadForumPost() {
         e.preventDefault();
 
         setHasSubbed(true);
+
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('text', text);
+
+        const newPost = await dispatch(createPostThunk(formData));
+
+        setTitle('');
+        setText('');
+
+        setValErrs([]);
+        setHasSubbed(false);
+
+        history.push(`/forum/posts/${newPost.id}`);
     }
 
     return (
