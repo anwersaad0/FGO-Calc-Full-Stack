855e25de8c056bb4dafd5e4dc0ec6a73f2ffc686
anwersaad0 <80143838+anwersaad0@users.noreply.github.com>
2024-10-11 11:30:12 -0700

    added useffect and DOM to editPost

--- react-app/src/components/EditForumPost/index.js
+++ react-app/src/components/EditForumPost/index.js
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { editPostThunk } from "../../store/forum";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function EditForumPost() {
+    const {postId} = useParams();
     const dispatch = useDispatch();
     const history = useHistory();
 
     const sessionUser = useSelector(state => state.session.user);
+    const forumPost = useSelector(state => state.posts[postId]);
+
+    useEffect(() => {
+        if (forumPost) {
+            if (!sessionUser || sessionUser.id !== forumPost.userId) {
+                history.push('/');
+            }
+        }
+    }, [forumPost, sessionUser, history]);
 
     const [text, setText] = useState("");
 
@@ -16,6 +26,14 @@ function EditForumPost() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        setHasSubbed(true);
+        if (valErrs.length) return alert('Your edits have errors, submit changes failed')
+
+        //const formData = new FormData();
+        //formData.append('text', text);
+
+        setText('');
     }
 
     if (!sessionUser) {
@@ -37,6 +55,40 @@ function EditForumPost() {
 
             </div>
 
+            {hasSubbed && valErrs.length > 0 && (
+                <div className="errors-text">
+                    <ul>
+                        {valErrs?.map(err => (
+                            <li key={err}>{err}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+
+            <form onSubmit={(e) => handleSubmit(e)} className="edit-post-details" >
+
+                <div className="edit-detail">
+                    <div></div>
+                    <textarea
+                        className="text-input"
+                        type="text"
+                        name="text"
+                        rows="40"
+                        cols="8"
+                        onChange={e => setText(e.target.value)}
+                        value={text}
+                        required={true}
+                    ></textarea>
+                </div>
+
+                <div className="edit-detail">
+                    <button className="confirm-edit-post" type="submit">Submit Edits</button>
+                </div>
+
+            </form>
+
         </main>
     )
 }
+
+export default EditForumPost;
