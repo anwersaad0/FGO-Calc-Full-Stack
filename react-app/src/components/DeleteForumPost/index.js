87d5065b01993490407fc2e3cc84f96540a4cbf6
anwersaad0 <anwer.saad0@gmail.com>
2024-10-27 17:25:31 -0700

    created delete forum post page

--- react-app/src/components/DeleteForumPost/index.js
+++ react-app/src/components/DeleteForumPost/index.js
@@ -1,21 +1,88 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { onePostThunk, deletePostThunk } from "../../store/forum";
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
 
-function DeletePost() {
-
+function DeleteForumPost() {
+    const { postId } = useParams();
     const dispatch = useDispatch();
+    const history = useHistory();
 
+    const sessionUser = useSelector(state => state.session.user);
+    const forumPost = useSelector(state => state.forum[postId]);
+
+    useEffect(() => {
+        dispatch(onePostThunk(postId));
+    }, [dispatch]);
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+
+        history.push('/forum/posts');
+
+        await deletePostThunk(postId);
+    }
+
+    if (!sessionUser) {
+        return (
+            <>
+
+                Must be signed in to delete a forum post.
+
+            </>
+        )
+    }
+
+    if (sessionUser?.id !== forumPost?.userId) {
+        return (
+            <>
+
+                You cannot delete posts from other users.
+
+            </>
+        )
+    }
 
     return (
+
         <main>
 
             <div>
 
-                
+                <h1> Delete this post? </h1>
+
+            </div>
+
+            <div>
+
+                <h2> Post Details </h2>
+
+                <div> Title: {forumPost?.title} </div>
+
+                <div> Contents: {forumPost?.text} </div>
+
+            </div>
+
+            <div>
+
+                <div>
+
+                    <button onClick={(e) => handleDelete(e)} className="confirm-delete-forum-post"> Yes, delete this post. </button>
+
+                </div>
+
+                <div>
+
+                    <NavLink exact to={`/forum/posts/${postId}`} className="decline-delete-forum-post"> No, keep the post. </NavLink>
+
+                </div>
 
             </div>
 
         </main>
+
     )
 }
+
+export default DeleteForumPost;
